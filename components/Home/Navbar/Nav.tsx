03cac0b732e43adfa18cbbab78ba592b7d0c3d5e
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-16 10:58:44 +0200

    Add nav bar scroll check

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,11 +1,32 @@
+'use client';
 import { Navlinks } from '@/constant/constant';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { HiBars3BottomRight } from 'react-icons/hi2';
 import { MdDeliveryDining } from 'react-icons/md';
 
 const Nav = () => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY < 90) setNavBg(false);
+    };
+
+    window.addEventListener('scroll', handler);
+
+    return () => {
+      window.removeEventListener('scroll', handler);
+    };
+  }, []);
+
   return (
-    <div className="transition-all duration-200 h-[12vh] z-[100] fixed w-full">
+    <div
+      className={`transition-all ${
+        navBg ? 'bg-white shadow-md' : 'fixed'
+      } duration-200 h-[12vh] z-[100] fixed w-full`}
+    >
       <div className="flex items-center justify-between h-full w-[90%] xl:w-[80%] mx-auto">
         <div className="flex items-center space-x-2">
           <div className="w-10 h-10 bg-blue-950 rounded-full flex flex-col items-center justify-center">
@@ -32,6 +53,7 @@ const Nav = () => {
           <button className="bg-blue-950 text-white font-bold px-8 py-2.5 rounded-lg hover:bg-black transition-all duration-300 cursor-pointer">
             Join Now
           </button>
+          <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-blue-950 lg:hidden" />
         </div>
       </div>
     </div>
