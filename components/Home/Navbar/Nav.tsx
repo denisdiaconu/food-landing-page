e95593fc5c573bbfd9440f931b3cd92a0eafacf3
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-17 10:10:03 +0200

    Add dark mode to all nav bar

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,4 +1,5 @@
 'use client';
+import ThemeToggler from '@/components/Helper/ThemeToggler';
 import { Navlinks } from '@/constant/constant';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
@@ -28,15 +29,15 @@ const Nav = ({ openNav }: Props) => {
   return (
     <div
       className={`transition-all ${
-        navBg ? 'bg-white shadow-md' : 'fixed'
+        navBg ? 'bg-white dark:bg-gray-900 shadow-md' : 'fixed'
       } duration-200 h-[12vh] z-[100] fixed w-full`}
     >
       <div className="flex items-center justify-between h-full w-[90%] xl:w-[80%] mx-auto">
         <div className="flex items-center space-x-2">
-          <div className="w-10 h-10 bg-blue-950 rounded-full flex flex-col items-center justify-center">
-            <MdDeliveryDining className="w-6 h-6 text-white" />
+          <div className="w-10 h-10 bg-blue-950 dark:bg-white rounded-full flex flex-col items-center justify-center">
+            <MdDeliveryDining className="w-6 h-6 text-white dark:text-black" />
           </div>
-          <h1 className="text-black font-bold text-xl hidden sm:block md:text-2xl">
+          <h1 className="text-black dark:text-white font-bold text-xl hidden sm:block md:text-2xl">
             Foodie
           </h1>
         </div>
@@ -46,7 +47,7 @@ const Nav = ({ openNav }: Props) => {
               <Link
                 key={link.id}
                 href={link.url}
-                className="text-black font-bold hover:text-green-700 transition-all duration-200"
+                className="text-black dark:text-white font-bold hover:text-green-700 dark:hover:text-green-400 transition-all duration-200"
               >
                 <p>{link.label}</p>
               </Link>
@@ -54,12 +55,13 @@ const Nav = ({ openNav }: Props) => {
           })}
         </div>
         <div className="flex items-center space-x-4">
-          <button className="bg-blue-950 text-white font-bold px-8 py-2.5 rounded-lg hover:bg-black transition-all duration-300 cursor-pointer">
+          <button className="bg-blue-950 dark:bg-white text-white dark:text-black font-bold px-8 py-2.5 rounded-lg hover:bg-black dark:hover:bg-gray-200 transition-all duration-300 cursor-pointer">
             Join Now
           </button>
+          <ThemeToggler />
           <HiBars3BottomRight
             onClick={openNav}
-            className="w-8 h-8 cursor-pointer text-blue-950 lg:hidden"
+            className="w-8 h-8 cursor-pointer text-blue-950 dark:text-white lg:hidden"
           />
         </div>
       </div>
