5df1e82a7384850141487f33fa5c1abdefd37e09
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-19 10:47:55 +0200

    Add restaurant card component

--- components/Home/Restaurant/Restaurant.tsx
+++ components/Home/Restaurant/Restaurant.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Restaurant = () => {
   return (
-    <div >Restaurant</div>
+    <div className='pt-16 pb-16'>
+        <div className='text-xl sm:text-2xl text-center font-extrabold'>
+            Available Restaurants Nearby
+        </div>
+        <div className='w-[80%] mx-auto grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-14'>
+
+        </div>
+    </div>
   )
 }
 
