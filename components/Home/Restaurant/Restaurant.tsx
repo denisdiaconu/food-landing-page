5eb3f6bbb5cdd86a891548f8bae47c0dcd36321a
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-19 10:59:04 +0200

    Add images and titles to restaurant

--- components/Home/Restaurant/Restaurant.tsx
+++ components/Home/Restaurant/Restaurant.tsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React from 'react';
+import RestaurantCard from './RestaurantCard';
 
 const Restaurant = () => {
   return (
-    <div className='pt-16 pb-16'>
-        <div className='text-xl sm:text-2xl text-center font-extrabold'>
-            Available Restaurants Nearby
+    <div className="pt-16 pb-16">
+      <div className="text-xl sm:text-2xl text-center font-extrabold">
+        Available Restaurants Nearby
+      </div>
+      <div className="w-[80%] mx-auto grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-14">
+        <div>
+          <RestaurantCard
+            image="/images/r1.jpg"
+            title="Pizza Hut Delicious Pizza"
+          />
         </div>
-        <div className='w-[80%] mx-auto grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-14'>
-
+        <div>
+          <RestaurantCard
+            image="/images/r2.jpg"
+            title="Chipotle Mexican Grill (2675 Geary Boulevard)"
+          />
         </div>
+        <div>
+          <RestaurantCard
+            image="/images/r3.jpg"
+            title="McDonald's® Burgers (Fillmore)"
+          />
+        </div>
+        <div>
+          <RestaurantCard
+            image="/images/r4.jpg"
+            title="The Baked Bear San Francisco"
+          />
+        </div>
+        <div>
+          <RestaurantCard
+            image="/images/r5.jpg"
+            title="Shake Shack (3060 Fillmore Street)"
+          />
+        </div>
+        <div>
+          <RestaurantCard
+            image="/images/r6.jpg"
+            title="Chubby Noodle Chinese Takeout"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Restaurant
+export default Restaurant;
--- components/Home/Restaurant/RestaurantCard.tsx
+++ components/Home/Restaurant/RestaurantCard.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-const RestaurantCard = () => {
+type Props = {
+  title: string;
+  image: string;
+};
+
+const RestaurantCard = ({ image, title }: Props) => {
   return (
-    <div>RestaurantCard</div>
-  )
-}
+    <div>
+      <Image
+        src={image}
+        alt="image"
+        width={400}
+        height={400}
+        className="w-full h-[250px] object-cover"
+      />
+      <h1 className='mt-4 w-fit hover:text-blue-950 dark:hover:text-amber-300 transition-all duration-200 cursor-pointer text-base sm:text-lg font-bold'>{title}</h1>
+    </div>
+  );
+};
 
-export default RestaurantCard
+export default RestaurantCard;
