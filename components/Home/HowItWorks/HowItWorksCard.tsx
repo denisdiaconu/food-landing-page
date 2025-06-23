f455534ed0a4dd9615a9046cc586b4be99af8a67
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-23 11:06:34 +0200

    Add how it works card content

--- components/Home/HowItWorks/HowItWorksCard.tsx
+++ components/Home/HowItWorks/HowItWorksCard.tsx
@@ -22,6 +22,15 @@ const HowItWorksCard = ({ num, image, title, description }: Props) => {
         <div className="w-14 h-14 bg-pink-600 text-white text-xl font-bold flex items-center justify-center flex-col absolute top-0 left-0 rounded-full">
           {num}
         </div>
+        <h1 className="mt-6 text-lg sm:text-xl font-bold text-center">
+          {title}
+        </h1>
+        <p className="text-gray-700 dark:text-gray-300 text-center sm:w-[80%] mx-auto mt-3 font-medium">
+          {description}
+        </p>
+        <p className="mt-4 text-base font-bold text-blue-800 dark:text-blue-400 hover:text-blue-600 transition-all duration-150 cursor-pointer text-center">
+          Start Earning &rarr;
+        </p>
       </div>
     </div>
   );
