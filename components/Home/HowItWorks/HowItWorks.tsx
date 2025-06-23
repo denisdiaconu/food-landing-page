a4154ca8cd35776567656c8ffe25da5ad99fdef7
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-23 10:31:03 +0200

    Add how it works component

---
+++ components/Home/HowItWorks/HowItWorks.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HowItWorks = () => {
+  return (
+    <div className="pt-16 pb-16">
+      <h1 className="text-xl sm:text-2xl text-center font-extrabold">
+        Let&apos;s see how it works
+      </h1>
+    </div>
+  );
+};
+
+export default HowItWorks;
