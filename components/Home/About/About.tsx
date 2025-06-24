e597a106ed1a28177b66274f2052328ee986c729
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-24 10:47:49 +0200

    Add image to about section

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="pt-16 pb-16">
+      <div className="w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
+        <div>
+          <Image src="/images/a.png" alt="image" width={800} height={800} />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
