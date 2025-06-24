1fb9a8d871816787fc4c709b8403a35099881703
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-24 10:52:14 +0200

    Add text to about section

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -8,7 +8,18 @@ const About = () => {
         <div>
           <Image src="/images/a.png" alt="image" width={800} height={800} />
         </div>
-        <div></div>
+        <div>
+          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-extrabold leading-8 md:leading-12">
+            Lightning-fast delivery — almost faster than Superman!
+          </h1>
+          <p className="mt-4 leading-6 sm:leading-8 text-gray-800 dark:text-gray-300 font-medium text-sm sm:text-base">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut unde
+            vitae, provident placeat dolorum dicta
+          </p>
+          <div>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
