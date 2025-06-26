d72110cf647fe231cf204aff0139fee8921a4f8f
denisdiaconu <denisdiaconu117@yahoo.com>
2025-06-26 10:02:28 +0200

    Add setup for react multi carousel

--- components/Home/ClientReview/ClientReview.tsx
+++ components/Home/ClientReview/ClientReview.tsx
@@ -1,4 +1,25 @@
+'use client';
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1224 },
+    items: 2,
+    slidesToSlide: 1
+  },
+  tablet: {
+    breakpoint: { max: 1224, min: 764 },
+    items: 1,
+    slidesToSlide: 1
+  },
+  mobile: {
+    breakpoint: { max: 764, min: 0 },
+    items: 1,
+    slidesToSlide: 1
+  }
+};
 
 const ClientReview = () => {
   return (
@@ -6,9 +27,7 @@ const ClientReview = () => {
       <h1 className="text-xl sm:text-2xl text-center font-extrabold">
         What People Say About Us
       </h1>
-      <div className='mt-16 w-[80%] mx-auto'>
-        
-      </div>
+      <div className="mt-16 w-[80%] mx-auto"></div>
     </div>
   );
 };
