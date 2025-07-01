7582e67ad3aefda28ff2258beb2bf32f0ea84aa0
denisdiaconu <denisdiaconu117@yahoo.com>
2025-07-01 10:48:54 +0200

    Add footer icons

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
+import { FaX } from 'react-icons/fa6';
+import { MdDeliveryDining } from 'react-icons/md';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="pt-16 pb-16 bg-gray-800">
+      <div className="w-[80%] mx-auto items-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10">
+        <div>
+          <div className="flex items-center space-x-2">
+            <div className="w-10 h-10 bg-white rounded-full flex flex-col items-center justify-center">
+              <MdDeliveryDining className="w-6 h-6 text-black" />
+            </div>
+            <h1 className="text-xl hidden sm:block md:text-2xl text-white font-bold">
+              Foodie
+            </h1>
+          </div>
+          <p className="mt-4 text-gray-200 font-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aut
+            quisquam libero.
+          </p>
+          <div className="mt-6 flex items-center space-x-2">
+            <div className="flex items-center justify-center flex-col w-8 h-8 bg-blue-600 text-white rounded-full">
+              <FaFacebookF />
+            </div>
+            <div className="flex items-center justify-center flex-col w-8 h-8 bg-pink-500 text-white rounded-full">
+              <FaInstagram />
+            </div>
+            <div className="flex items-center justify-center flex-col w-8 h-8 bg-gray-500 text-white rounded-full">
+              <FaX />
+            </div>
+            <div className="flex items-center justify-center flex-col w-8 h-8 bg-red-600 text-white rounded-full">
+              <FaYoutube />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
