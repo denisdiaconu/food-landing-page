bdce0a331138accf0436201cd2eb4fe78071a4f5
denisdiaconu <denisdiaconu117@yahoo.com>
2025-07-03 10:47:09 +0200

    Add aos config

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from './Hero/Hero';
 import Restaurant from './Restaurant/Restaurant';
 import Category from './Category/Category';
@@ -12,6 +12,19 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 const Home = () => {
+  useEffect(() => {
+    const initAOS = async () => {
+      await import('aos');
+      AOS.init({
+        duration: 1000,
+        easing: 'ease',
+        once: true,
+        anchorPlacement: 'top-bottom',
+      });
+    };
+    initAOS();
+  }, []);
+
   return (
     <div className="overflow-hidden">
       <Hero />
