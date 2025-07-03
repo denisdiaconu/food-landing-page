c258500536624db638ca6ca5c70915f0c0e20839
denisdiaconu <denisdiaconu117@yahoo.com>
2025-07-03 10:43:59 +0200

    Add aos type

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Hero from './Hero/Hero'
-import Restaurant from './Restaurant/Restaurant'
-import Category from './Category/Category'
-import HowItWorks from './HowItWorks/HowItWorks'
-import About from './About/About'
-import Feature from './Feature/Feature'
-import ClientReview from './ClientReview/ClientReview'
-import MobileApp from './MobileApp/MobileApp'
+'use client';
+import React from 'react';
+import Hero from './Hero/Hero';
+import Restaurant from './Restaurant/Restaurant';
+import Category from './Category/Category';
+import HowItWorks from './HowItWorks/HowItWorks';
+import About from './About/About';
+import Feature from './Feature/Feature';
+import ClientReview from './ClientReview/ClientReview';
+import MobileApp from './MobileApp/MobileApp';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
   return (
-    <div className='overflow-hidden'>
+    <div className="overflow-hidden">
       <Hero />
       <Restaurant />
       <Category />
@@ -20,7 +23,7 @@ const Home = () => {
       <ClientReview />
       <MobileApp />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
